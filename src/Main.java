115ccf6bce7c32785b1342dee6683e6f65fb235e
darcyp <eelakoopfm@gmail.com>
2023-02-21 18:01:41 +0200

    task7

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+
+    public static int square(int a) {
+        int squareA = a * a;
+        return squareA;
+    }
+
+    public static void main(String[] args) {
+        int a = square(10);
+        System.out.println(a);
+    }
+}
